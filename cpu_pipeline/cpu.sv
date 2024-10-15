03cc54366bc055ad5b14252db60e841da0dd05eb
Juan Montero <jjmontero9@gmail.com>
2024-10-15 16:27:51 -0600

    Pipeline: Data path

--- cpu_pipeline/cpu.sv
+++ cpu_pipeline/cpu.sv
@@ -10,13 +10,13 @@ module cpu (
     output logic MemWrite
 );
 
-    logic       PCSrcD;
-    logic [1:0] ResultSrcD;
-    logic [2:0] ALUControlD;
-    logic       ALUSrcD;
-    logic       MemWriteD;
-    logic [1:0] ImmSrcD;
-    logic       RegWriteD;
+    logic       PCSrc;
+    logic [1:0] ResultSrc;
+    logic [2:0] ALUControl;
+    logic       ALUSrc;
+    logic       MemWrite;
+    logic [1:0] ImmSrc;
+    logic       RegWrite;
     logic       zero;
 
     datapath data1 (
@@ -24,12 +24,12 @@ module cpu (
         .rst         (rst),
         .Instr       (Instr),
         .ReadData    (ReadData),
-        .PCSrc       (PCSrcD),
-        .ResultSrc   (ResultSrcD),
-        .ALUControl  (ALUControlD),
-        .ALUSrc      (ALUSrcD),
-        .ImmSrc      (ImmSrcD),
-        .RegWrite    (RegWriteD),
+        .PCSrc       (PCSrc),
+        .ResultSrc   (ResultSrc),
+        .ALUControl  (ALUControl),
+        .ALUSrc      (ALUSrc),
+        .ImmSrc      (ImmSrc),
+        .RegWrite    (RegWrite),
         .PC          (PC),
         .ALUResult   (ALUResult),
         .WriteData   (WriteData),
@@ -41,13 +41,13 @@ module cpu (
         .funct3      (Instr[14:12]),
         .funct7_bit5 (Instr[30]),
         .Zero        (zero),
-        .PCSrc       (PCSrcD),
-        .ResultSrc   (ResultSrcD),
-        .MemWrite    (MemWriteD),
-        .ALUControl  (ALUControlD),
-        .ALUSrc      (ALUSrcD),
-        .ImmSrc      (ImmSrcD),
-        .RegWrite    (RegWriteD)
+        .PCSrc       (PCSrc),
+        .ResultSrc   (ResultSrc),
+        .MemWrite    (MemWrite),
+        .ALUControl  (ALUControl),
+        .ALUSrc      (ALUSrc),
+        .ImmSrc      (ImmSrc),
+        .RegWrite    (RegWrite)
     );
 
 endmodule : cpu
@@ -69,6 +69,144 @@ module datapath (
     output logic [31:0] WriteData,
     output logic        zero
 );
+
+    mux32 mux_pc_src (
+        .sel (PCSrc),
+        .A   (PCPlus4F),
+        .B   (PCTargetE),
+        .Q   (PCFprime)
+    );
+
+    pc pc1 (
+        .clk    (clk),
+        .rst    (rst),
+        .en     (1'b1),
+        .PCNext (PCFprime),
+        .PC     (PCF)
+    );
+
+    adder32 add_pc_plus_4 (
+        .A       (PCF),
+        .B       (4),
+        .Q       (PCPlus4F)
+    );
+
+    // Conexiones a la memoria imem externa
+    assign PC = PCF;
+    assign InstrF = Instr;
+
+    pipe_reg_D prD1 (
+        .clk      (clk),
+        .rst      (rst),
+        .PCF      (PCF),
+        .InstrF   (InstrF),
+        .PCPlus4F (PCPlus4F),
+        .PCD      (PCD),
+        .InstrD   (InstrD),
+        .PCPlus4D (PCPlus4D)
+    );
+
+    register_bank rb1 (
+        .clk (clk),
+        .rst (rst),
+        .WE3 (RegWrite),
+        .A1  (InstrD[19:15]),
+        .A2  (InstrD[24:20]),
+        .A3  (RdW),
+        .WD3 (ResultW),
+        .RD1 (RD1D),
+        .RD2 (RD2D)
+    );
+
+    assign RdD = InstrD[11:7];
+
+    Extend ext1 (
+        .src (ImmSrc),
+        .A   (InstrD),
+        .Q   (ImmExtD)
+    );
+
+    pipe_reg_E prE1 (
+        .clk      (clk),
+        .rst      (rst),
+        .RD1D     (RD1D),
+        .RD2D     (RD2D),
+        .PCD      (PCD),
+        .RdD      (RdD),
+        .ImmExtD  (ImmExtD),
+        .PCPlus4D (PCPlus4D),
+        .RD1E     (RD1E),
+        .RD2E     (RD2E),
+        .PCE      (PCE),
+        .RdE      (RdE),
+        .ImmExtE  (ImmExtE),
+        .PCPlus4E (PCPlus4E)
+    );
+
+    mux32 mux_SrcB (
+        .sel  (ALUSrc),
+        .A    (RD2E),
+        .B    (ImmExtE),
+        .Q    (SrcBE)
+    );
+
+    adder32 add_pc_target (
+        .A       (PCE),
+        .B       (ImmExtE),
+        .Q       (PCTargetE)
+    );
+
+    alu alu1 (
+        .ALUControl (ALUControl),
+        .A          (SrcAE),
+        .B          (SrcBE),
+        .Result     (ALUResultE),
+        .oVerflow   (),
+        .Carry      (),
+        .Negative   (),
+        .Zero       (Zero)
+    );
+
+    assign WriteDataE = RD2E;
+
+    pipe_reg_M prM1 (
+        .clk         (clk),
+        .rst         (rst),
+        .ALUResultE  (ALUResultE),
+        .WriteDataE  (WriteDataE),
+        .RdE         (RdE),
+        .PCTargetE   (PCTargetE),
+        .ALUResultM  (ALUResultM),
+        .WriteDataM  (WriteDataM),
+        .RdM         (RdM),
+        .PCTargetM   (PCTargetM)
+    );
+
+    assign ALUResult = ALUResultM;
+    assign WriteData = WriteDataM;
+    assign ReadDataM = ReadData;
+
+    pipe_reg_W prW1 (
+        .clk        (clk),
+        .rst        (rst),
+        .ALUResultM (ALUResultM),
+        .ReadDataM  (ReadDataM),
+        .RdM        (RdM),
+        .PCPlus4M   (PCPlus4M),
+        .ALUResultW (ALUResultW),
+        .ReadDataW  (ReadDataW),
+        .RdW        (RdW),
+        .PCPlus4W   (PCPlus4W)
+    );
+
+    module mux32_4 (
+        .sel  (ResultSrc),
+        .A    (ALUResultW),
+        .B    (ReadDataW),
+        .C    (PCPlus4W),
+        .D    (0),
+        .Q    (ResultW) 
+    );
 
 endmodule : datapath
 
@@ -318,28 +456,201 @@ endmodule : adder32
 
 
 module pipe_reg_D (
-    
+    input clk,
+    input rst,
+    input [31:0] PCF,
+    input [31:0] InstrF,
+    input [31:0] PCPlus4F,
+    output logic [31:0] PCD,
+    output logic [31:0] InstrD,
+    output logic [31:0] PCPlus4D
 );
+
+    reg_n reg_PC #(.bits(32)) (
+        .clk  (clk),
+        .rst  (rst),
+        .en   (1'b1),
+        .din  (PCF),
+        .dout (PCD)
+    );
+
+    reg_n reg_Instr #(.bits(32)) (
+        .clk  (clk),
+        .rst  (rst),
+        .en   (1'b1),
+        .din  (InstrF),
+        .dout (InstrD)
+    );
+
+    reg_n reg_PCPlus4 #(.bits(32)) (
+        .clk  (clk),
+        .rst  (rst),
+        .en   (1'b1),
+        .din  (PCPlus4F),
+        .dout (PCPlus4D)
+    );
 
 endmodule : pipe_reg_D
 
 
 module pipe_reg_E (
-
+    input        clk,
+    input        rst,
+    input [31:0] RD1D,
+    input [31:0] RD2D,
+    input [31:0] PCD,
+    input [31:0] RdD,
+    input [31:0] ImmExtD,
+    input [31:0] PCPlus4D,
+    output logic [31:0] RD1E,
+    output logic [31:0] RD2E,
+    output logic [31:0] PCE,
+    output logic [31:0] RdE,
+    output logic [31:0] ImmExtE,
+    output logic [31:0] PCPlus4E
 );
+
+    reg_n reg_RD1 #(.bits(32)) (
+        .clk  (clk),
+        .rst  (rst),
+        .en   (1'b1),
+        .din  (RD1D),
+        .dout (RD1E)
+    );
+
+    reg_n reg_RD2 #(.bits(32)) (
+        .clk  (clk),
+        .rst  (rst),
+        .en   (1'b1),
+        .din  (RD2D),
+        .dout (RD2E)
+    );
+
+    reg_n reg_PC #(.bits(32)) (
+        .clk  (clk),
+        .rst  (rst),
+        .en   (1'b1),
+        .din  (PCD),
+        .dout (PCE)
+    );
+
+    reg_n reg_Rd #(.bits(32)) (
+        .clk  (clk),
+        .rst  (rst),
+        .en   (1'b1),
+        .din  (RdD),
+        .dout (RdE)
+    );
+
+    reg_n reg_ImmExt #(.bits(32)) (
+        .clk  (clk),
+        .rst  (rst),
+        .en   (1'b1),
+        .din  (ImmExtD),
+        .dout (ImmExtE)
+    );
+
+    reg_n reg_PCPlus4 #(.bits(32)) (
+        .clk  (clk),
+        .rst  (rst),
+        .en   (1'b1),
+        .din  (PCPlus4D),
+        .dout (PCPlus4E)
+    );
 
 endmodule : pipe_reg_E
 
 
 module pipe_reg_M (
-
+    input clk,
+    input rst,
+    input [31:0] ALUResultE,
+    input [31:0] WriteDataE,
+    input [31:0] RdE,
+    input [31:0] PCTargetE,
+    output logic [31:0] ALUResultM,
+    output logic [31:0] WriteDataM,
+    output logic [31:0] RdM,
+    output logic [31:0] PCTargetM
 );
 
+    reg_n reg_ALUResult #(.bits(32)) (
+        .clk  (clk),
+        .rst  (rst),
+        .en   (1'b1),
+        .din  (ALUResultE),
+        .dout (ALUResultM)
+    );
+
+    reg_n reg_WriteData #(.bits(32)) (
+        .clk  (clk),
+        .rst  (rst),
+        .en   (1'b1),
+        .din  (WriteDataE),
+        .dout (WriteDataM)
+    );
+
+    reg_n reg_Rd #(.bits(32)) (
+        .clk  (clk),
+        .rst  (rst),
+        .en   (1'b1),
+        .din  (RdE),
+        .dout (RdM)
+    );
+
+    reg_n reg_PCTarget #(.bits(32)) (
+        .clk  (clk),
+        .rst  (rst),
+        .en   (1'b1),
+        .din  (PCTargetE),
+        .dout (PCTargetM)
+    );
 
 endmodule : pipe_reg_M
 
 module pipe_reg_W (
-
+    input clk,
+    input rst,
+    input [31:0] ALUResultM,
+    input [31:0] ReadDataM,
+    input [31:0] RdM,
+    input [31:0] PCPlus4M,
+    output logic [31:0] ALUResultW,
+    output logic [31:0] ReadDataW,
+    output logic [31:0] RdW,
+    output logic [31:0] PCPlus4W
 );
+
+    reg_n reg_ALUResult #(.bits(32)) (
+        .clk  (clk),
+        .rst  (rst),
+        .en   (1'b1),
+        .din  (ALUResultM),
+        .dout (ALUResultW)
+    );
+
+    reg_n reg_ReadData #(.bits(32)) (
+        .clk  (clk),
+        .rst  (rst),
+        .en   (1'b1),
+        .din  (ReadDataM),
+        .dout (ReadDataW)
+    );
+
+    reg_n reg_Rd #(.bits(32)) (
+        .clk  (clk),
+        .rst  (rst),
+        .en   (1'b1),
+        .din  (RdM),
+        .dout (RdW)
+    );
+
+    reg_n reg_PCPlus4 #(.bits(32)) (
+        .clk  (clk),
+        .rst  (rst),
+        .en   (1'b1),
+        .din  (PCPlus4M),
+        .dout (PCPlus4W)
+    );
 
 endmodule : pipe_reg_W
