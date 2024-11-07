f37e9fd227918a65a1f5ba10f3f1b53d38b8ace3
Juan Montero <jjmontero9@gmail.com>
2024-11-07 11:37:36 -0600

    Pipeline: bug fixes

--- cpu_pipeline/cpu.sv
+++ cpu_pipeline/cpu.sv
@@ -518,6 +518,18 @@ module Extend (
     output logic [31:0] Q
 );
 
+    always_comb begin
+
+        case (src)
+            2'b00:   Q = {{20{A[31]}}, A[31:20]};                            // I-Type
+            2'b01:   Q = {{20{A[31]}}, A[31:25], A[11:7]};                   // S-Type
+            2'b10:   Q = {{19{A[31]}}, A[31], A[7],A[30:25], A[11:8], 1'b0}; // B-Type
+            2'b11:   Q = {{12{A[31]}}, A[19:12], A[20], A[30:21], 1'b0};     // J-Type
+            //default: Q = 32'hDEADBEEF; // error
+        endcase
+
+    end
+
 endmodule : Extend
 
 
