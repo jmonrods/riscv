a29fba06b66697a1b82952950e389d4c50de73e3
Juan Montero <jjmontero9@gmail.com>
2024-11-07 12:24:21 -0600

    Quick fixes

--- cpu_pipeline/cpu.sv
+++ cpu_pipeline/cpu.sv
@@ -951,15 +951,15 @@ module hazard_unit (
 );
 
     always_comb begin
-        if ((Rs1E == RdM) & RegWriteM & (Rs1E != 0))      ForwardAE = 10;
-        else if ((Rs1E == RdW) & RegWriteW & (Rs1E != 0)) ForwardAE = 01;
-        else                                              ForwardAE = 00;
+        if ((Rs1E == RdM) & RegWriteM & (Rs1E != 0))      ForwardAE = 2'b10;
+        else if ((Rs1E == RdW) & RegWriteW & (Rs1E != 0)) ForwardAE = 2'b01;
+        else                                              ForwardAE = 2'b00;
     end
 
     always_comb begin
-        if ((Rs2E == RdM) & RegWriteM & (Rs2E != 0))      ForwardBE = 10;
-        else if ((Rs2E == RdW) & RegWriteW & (Rs2E != 0)) ForwardBE = 01;
-        else                                              ForwardBE = 00;
+        if ((Rs2E == RdM) & RegWriteM & (Rs2E != 0))      ForwardBE = 2'b10;
+        else if ((Rs2E == RdW) & RegWriteW & (Rs2E != 0)) ForwardBE = 2'b01;
+        else                                              ForwardBE = 2'b00;
     end
 
 endmodule : hazard_unit
